e045de4fc3b51cce7d701367c91845e7b23d6818
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-06-08 11:55:24 +0800

    Add init TextView and init seekBar

--- app/src/main/java/com/suhun/seekbarservice/MainActivity.java
+++ app/src/main/java/com/suhun/seekbarservice/MainActivity.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String tag = MainActivity.class.getSimpleName();
+    private TextView showCounterText;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+    private void initView(){
+        showCounterText = findViewById(R.id.showCounter);
+        seekBar = findViewById(R.id.seek);
     }
 
     public void startServiceFun(View view){
